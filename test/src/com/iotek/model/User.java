c15775987727e73509b65444d8dff69fc217f96a
xhj02130398 <50480572+xhj02130398@users.noreply.github.com>
2019-05-10 17:12:18 +0800

    Update User.java

--- test/src/com/iotek/model/User.java
+++ test/src/com/iotek/model/User.java
@@ -3,6 +3,7 @@ package com.iotek.model;
 public class User {
     public static void main(String[] args) {
         System.out.println("Hello,word!");
+        System.out.println("123!");
     }
 
 }
